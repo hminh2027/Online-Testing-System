bf95bd07ff72436c6bc089933a06ef875298c601
WIN-5RTH57NC1MN\Administrator <vhminh2027@gmail.com>
2023-05-06 18:02:14 +0700

    hotfix

--- api/src/prototypes/user.prototype.js
+++ api/src/prototypes/user.prototype.js
@@ -3,7 +3,7 @@ const { faker } = require("@faker-js/faker");
 function userPrototype(index) {
   return {
     email: `vhminh202${7 + index}@gmail.com`,
-    name: faker.name.lastName(),
+    name: faker.name.fullName(),
     password: "123456",
   };
 }
