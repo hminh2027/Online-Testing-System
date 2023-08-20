52a14f5e2ea67596d66f7b4103c07f885b3b4aba
hminh2027 <dackheros@gmail.com>
2023-08-20 21:18:43 +0700

    feat: add customized hook from react-query

--- client/src/hooks/useCustomQuery/index.ts
+++ client/src/hooks/useCustomQuery/index.ts
@@ -1,5 +1,57 @@
+import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
-export const initialCustomQUery = (service) => {
-  const useItem = (id, options) => useQuery([service.path, id], () => fetch);
+import { fetchItem, fetchList } from '@/hooks/useCustomQuery/fetchQuery';
+import type { Resource, ResponseItem } from '@/models/common';
+import type { Service } from '@/types/service';
+
+type ResponseList = ResponseItem<Resource[]>;
+type ResponseDetail = ResponseItem<Resource[]>;
+type ResponseModify = ResponseItem<Resource>;
+type RequestQuery = QueryParams | undefined | null;
+export type UseCustomQueryOptions<T> = Omit<
+  UseQueryOptions<T>,
+  'queryKey' | 'queryFn' | 'initialData'
+> & { initialData?: () => undefined };
+
+export const initialCustomQuery = <
+  X extends Resource,
+  Y extends ResponseList,
+  Z extends ResponseDetail,
+  W extends ResponseModify,
+  T extends RequestQuery,
+>(
+  service: Service,
+) => {
+  const useItem = (
+    id: number,
+    options?: UseCustomQueryOptions<Z>,
+  ): UseQueryResult<Z, unknown> =>
+    useQuery(
+      [service.path, id],
+      () =>
+        fetchItem<Z>({
+          id,
+          url: service.path,
+        }),
+      {
+        refetchOnWindowFocus: false,
+        enabled: !!id,
+        ...options,
+      },
+    );
+
+  const useList = (params: T, options?: UseCustomQueryOptions<Y>) =>
+    useQuery<Y>(
+      [service.path, params],
+      () =>
+        fetchList<T, Y>({
+          params,
+          url: service.path,
+        }),
+      {
+        refetchOnWindowFocus: false,
+        ...options,
+      },
+    );
 };
---
+++ client/src/models/common.ts
@@ -0,0 +1,18 @@
+import type { defaultParams } from '@/types/params';
+
+export interface Resource {
+  id?: string;
+}
+
+export interface Pagination {
+  page: number;
+  limit: number;
+  size: number;
+}
+
+export interface ResponseItem<T> {
+  message: string;
+  content: T;
+}
+
+export type QueryParams = typeof defaultParams;
