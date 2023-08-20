973eaaf6faea9895e9a7800e6cfba3d39b7f26fa
hminh2027 <dackheros@gmail.com>
2023-08-20 14:56:40 +0700

    feat: setup eslint airbnb rules + prettier format

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,20 +1,74 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2020: true,
   },
-}
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/typescript',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: [
+    'eslint-comments',
+    'import-newlines',
+    'promise',
+    'simple-import-sort',
+    '@typescript-eslint',
+    'react-refresh',
+  ],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    // ... (the rest of your rules)
+    'import/extensions': ['error', 'ignorePackages', { '': 'never' }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        // Allow CJS until ESM support improves
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+      },
+    },
+    {
+      files: '.eslintrc.json',
+      env: {
+        node: true,
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: '/client/tsconfig.json',
+      },
+    },
+  },
+};
