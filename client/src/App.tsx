e873bbba07c4968eb699008bf2da715090a339f4
hminh2027 <dackheros@gmail.com>
2023-08-20 21:19:11 +0700

    feat: init react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <QueryParamProvider adapter={ReactRouter6Adapter}>div</QueryParamProvider>
+    ),
+    path: '/',
+  },
+]);
 
 function App() {
-  return <>a</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
