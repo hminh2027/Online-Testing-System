dcb362041180f2d7d4dd46a609de4e0da701d126
hminh2027 <dackheros@gmail.com>
2023-08-21 22:09:25 +0700

    feat: init react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,44 @@
+import { Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 
+import MainLayout from './layouts/MainLayout';
+
 const router = createBrowserRouter([
   {
     element: (
-      <QueryParamProvider adapter={ReactRouter6Adapter}>div</QueryParamProvider>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
+        <MainLayout menuItems={{}} />
+      </QueryParamProvider>
     ),
-    path: '/',
+    children: [
+      {
+        path: 'login',
+        element: <div>this is login</div>,
+      },
+      {
+        path: 'signup',
+        element: <div>this is signup</div>,
+      },
+      {
+        index: true,
+        element: <div>defaulT</div>,
+      },
+      {
+        path: '*',
+        element: <div>404 | Page not found</div>,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
