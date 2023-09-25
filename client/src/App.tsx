23c1bf5d47ad4cd8bae85de1e8a3582c9298b4d5
hminh2027 <dackheros@gmail.com>
2023-09-25 11:11:32 +0700

    refactor: separate providers from app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,76 +1,13 @@
-import { Suspense } from 'react';
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-
-import { ConfigProvider, Spin } from 'antd';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ErrorBoundary } from 'react-error-boundary';
+import AppProvider from './providers';
 import 'antd/dist/reset.css';
 import '@/App.css';
-import { HelmetProvider } from 'react-helmet-async';
-import { themeConfig } from '@/config';
-
-import { AppRoutes } from '@/constants/path';
-import { ErrorPage } from './features/error';
-import { MainLayout } from './layouts';
-import * as AuthPages from '@/features/auth/pages';
-import { ClassRoute } from './features/class/routes';
-
-const router = createBrowserRouter([
-  {
-    ErrorBoundary: ErrorPage,
-    hasErrorBoundary: true,
-    children: [
-      {
-        path: 'login',
-        element: <AuthPages.Login />,
-      },
-      {
-        path: 'signup',
-        element: <AuthPages.SignUp />,
-      },
-      {
-        element: <MainLayout />,
-        children: [
-          {
-            path: 'class',
-            children: ClassRoute,
-          },
-        ],
-      },
-      {
-        index: true,
-        element: <Navigate to={AppRoutes.Login} replace />,
-      },
-    ],
-  },
-]);
-
-const queryClient = new QueryClient();
-
-ConfigProvider.config({
-  theme: {
-    token: {
-      ...themeConfig.token,
-    },
-    components: {
-      ...themeConfig.components,
-    },
-  },
-});
+import { AppRoutes } from './routes';
 
 function App() {
   return (
-    <ErrorBoundary FallbackComponent={ErrorPage}>
-      <Suspense fallback={<Spin />}>
-        <HelmetProvider>
-          <ConfigProvider theme={themeConfig}>
-            <QueryClientProvider client={queryClient}>
-              <RouterProvider router={router} />
-            </QueryClientProvider>
-          </ConfigProvider>
-        </HelmetProvider>
-      </Suspense>
-    </ErrorBoundary>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
   );
 }
 
---
+++ client/src/providers/index.tsx
@@ -0,0 +1,37 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Spin, ConfigProvider } from 'antd';
+import { Suspense, type PropsWithChildren } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import { ErrorPage } from '@/features/error';
+import { themeConfig } from '@/config';
+
+const queryClient = new QueryClient();
+
+ConfigProvider.config({
+  theme: {
+    token: {
+      ...themeConfig.token,
+    },
+    components: {
+      ...themeConfig.components,
+    },
+  },
+});
+
+export default function AppProvider({ children }: PropsWithChildren) {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <Suspense fallback={<Spin />}>
+        <HelmetProvider>
+          <ConfigProvider theme={themeConfig}>
+            <QueryClientProvider client={queryClient}>
+              <BrowserRouter>{children}</BrowserRouter>
+            </QueryClientProvider>
+          </ConfigProvider>
+        </HelmetProvider>
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
