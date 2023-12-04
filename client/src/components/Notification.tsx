806bf1a8156d01be9ac6d86e02540a77c635f68c
hminh2027 <dackheros@gmail.com>
2023-12-04 22:57:42 +0700

    feat: implement notification drawer

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -1,21 +1,31 @@
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Avatar, Badge, Dropdown } from 'antd';
+import { Avatar, Badge, Drawer, Dropdown } from 'antd';
+import { useToggle } from 'react-use';
 import { useListNotification } from '@/features/notification/hooks/useNotification';
 import { LoadingModal } from './LoadingModal';
 
 export function Notification() {
   const { data, isFetching } = useListNotification({});
+  const [open, toggleOpen] = useToggle(false);
 
   const notiList = data?.content;
 
   if (isFetching) return <LoadingModal />;
 
   return (
-    <Dropdown trigger={['click']}>
+    <>
       <Badge count={notiList?.length} overflowCount={99}>
-        <Avatar icon={<FontAwesomeIcon icon={faBell} />} shape="circle" size="large" />
+        <Avatar
+          onClick={toggleOpen}
+          icon={<FontAwesomeIcon icon={faBell} />}
+          shape="circle"
+          size="large"
+        />
       </Badge>
-    </Dropdown>
+      <Drawer title="Multi-level drawer" width={520} closable onClose={toggleOpen} open={open}>
+        hello
+      </Drawer>
+    </>
   );
 }
