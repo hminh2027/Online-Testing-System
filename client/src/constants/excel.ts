335d84a904c03392a29d3f8530deab1c9e90aad3
hminh2027 <dackheros@gmail.com>
2024-01-06 21:50:59 +0700

    feat: clean data

--- client/src/constants/excel.ts
+++ client/src/constants/excel.ts
@@ -1,8 +1,5 @@
 export const excelUrls = {
-  template: {
-    questions:
-      'https://res.cloudinary.com/minh2027/raw/upload/v1699628359/Testing%20Folder/ols-question-sample-excel_xaaf2r.xlsx',
-    studentList:
-      'https://res.cloudinary.com/minh2027/raw/upload/v1701528384/ols-student-sample-excel_y0jpsr.xlsx',
-  },
+  questions: '/template-question.xlsx',
+  studentList:
+    'https://res.cloudinary.com/minh2027/raw/upload/v1701528384/ols-student-sample-excel_y0jpsr.xlsx',
 };
