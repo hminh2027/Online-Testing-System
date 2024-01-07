d5f692b36bf4bb6be626e6180a1b60e64626caed
hminh2027 <dackheros@gmail.com>
2024-01-07 18:41:54 +0700

    feat: serve public folder

--- client/src/constants/excel.ts
+++ client/src/constants/excel.ts
@@ -1,5 +1,4 @@
 export const excelUrls = {
-  questions: '/template-question.xlsx',
-  studentList:
-    'https://res.cloudinary.com/minh2027/raw/upload/v1701528384/ols-student-sample-excel_y0jpsr.xlsx',
+  questions: '/template-questions.xlsx',
+  studentList: '',
 };
